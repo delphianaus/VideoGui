a0b26530653216af5f110564943e6ecbad8b6021
Justin Smyth <justin@smythconsulting.net>
2024-09-06 21:00:25 +1000

    fb

--- IEncryptionModule.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VideoGui
-{
-    public interface IEncryptionModule
-    {
-        string decryptrc4(byte[] input);
-        byte[] EncryptRC4(byte[] data);
-        byte[] RC4(byte[] bytes, byte[] key);
-    }
-}
