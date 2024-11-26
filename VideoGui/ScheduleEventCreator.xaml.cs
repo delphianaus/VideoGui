21d12d798f5a492ccd4c948e9cb0ba1cdee67966
Justin Smyth <justin@smythconsulting.net>
2024-11-26 11:24:00 +1100

    updates

--- VideoGui/ScheduleEventCreator.xaml.cs
+++ VideoGui/ScheduleEventCreator.xaml.cs
@@ -104,7 +104,19 @@ namespace VideoGui
         }
 
         
-
+        public void ChangeSource(bool EventPresent)
+        {
+            try
+            {
+                UpdateImage.Source = (EventPresent) ? 
+                    new BitmapImage(new Uri("pack://application:,,,/Icons/UpdateNew.png"))
+                    : new BitmapImage(new Uri("pack://application:,,,/Icons/UpdateCircle.png"));
+            }
+            catch (Exception ex)
+            {
+                ex.LogWrite($"ChangeSource {MethodBase.GetCurrentMethod()?.Name} {ex.Message}");
+            }
+        }
         private void lstSchedules_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
 
