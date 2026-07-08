bb16a2e904f8446039e0985a6ef9fb666b40249e
delphianaus <35714916+delphianaus@users.noreply.github.com>
2026-07-08 15:02:58 +1000

    updates

---
+++ VideoGui/Models/DataDiscover.cs
@@ -0,0 +1,25 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoGui.Models
+{
+    public class DataDiscover
+    {
+        public string TableName { get; set; } = "";
+        public List<string> Fields { get; set; } = new List<string>();
+        public DataDiscover(string tableName)
+        {
+            TableName = tableName;
+        }
+
+        public DataDiscover(FbDataReader reader)
+        {
+            TableName = reader.GetString(0).ToString();
+            Fields = new List<string>();
+        }
+    }
+}
